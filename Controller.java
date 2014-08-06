4408f9c5222a1a0076ee4a2495c041f084322154
sok891111 <sok891111@gmail.com>
2014-08-06 17:16:02 +0900

    Create Controller.java

---
+++ Controller.java
@@ -0,0 +1,47 @@
+package crawler;
+
+import java.util.ArrayList;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+public class Controller {
+    public static void main(String[] args) throws Exception {
+        String crawlStorageFolder = "/data/crawl/root";
+        int numberOfCrawlers = 1;
+        System.out.println(args);
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+        config.setMaxDepthOfCrawling(2);
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        robotstxtConfig.setEnabled(false);
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        /*
+         * For each crawl, you need to add some seed urls. These are the first
+         * URLs that are fetched and then the crawler starts following links
+         * which are found in these pages
+         */
+    	//controller.addSeed("http://www.ics.uci.edu/");
+		controller.addSeed("http://movie.naver.com/movie/bi/mi/pointWriteFormList.nhn?code=10046&type=after&isActualPointWriteExecute=false&isMileageSubscriptionAlready=false&isMileageSubscriptionReject=false");
+		
+		//controller.addSeed("http://www.ics.uci.edu/~welling/");
+         //controller.addSeed("http://movie.naver.com/movie/bi/mi/basic.nhn?code=10046");
+        //controller.addSeed("http://www.ics.uci.edu/~lopes/");
+        //controller.addSeed("http://www.ics.uci.edu/");
+		
+        /*
+         * Start the crawl. This is a blocking operation, meaning that your code
+         * will reach the line after this only when crawling is finished.
+         */
+        controller.start(MyCrawler.class, numberOfCrawlers);    
+}
+}
